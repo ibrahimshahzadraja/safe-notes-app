81c4126114a7abc320f6e4c8638f9c3ea92c978a
ibrahim <ibrahimdvp1@gmail.com>
2024-09-01 22:35:46 +0500

    fix url

--- frontend/utils.js
+++ frontend/utils.js
@@ -8,5 +8,4 @@ export const handleError = (msg) => {
     toast.error(msg, {position: 'top-right'})
 }
 
-// export const backend_url = "https://safe-notes-app.vercel.app/api/v1";
-export const backend_url = "http://localhost:8000/api/v1";
+export const backend_url = "https://safe-notes-app.vercel.app/api/v1";
